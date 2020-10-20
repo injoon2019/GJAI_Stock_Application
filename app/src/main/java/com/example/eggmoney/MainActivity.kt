c35293ed85449d4b6809f1813353082f6c82c9e6
unknown <kbp4154@naver.com>
2020-10-20 18:59:59 +0900

    kakao 로그인 버튼

--- app/src/main/java/com/example/eggmoney/MainActivity.kt
+++ app/src/main/java/com/example/eggmoney/MainActivity.kt
@@ -11,6 +11,8 @@ val SPLASH_VIEW_TIME : Long = 1500
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
         Handler().postDelayed({
             startActivity(Intent(this, SplashActivity::class.java))
