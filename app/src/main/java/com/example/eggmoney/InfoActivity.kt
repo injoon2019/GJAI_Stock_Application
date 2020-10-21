3e52d3773d30e7ee2ad8958744cf8da8313f2d6b
Injun <ison@sfu.ca>
2020-10-21 15:59:17 +0900

    InfoActivity에 Signout 버튼 추가

--- app/src/main/java/com/example/eggmoney/InfoActivity.kt
+++ app/src/main/java/com/example/eggmoney/InfoActivity.kt
@@ -2,10 +2,48 @@ package com.example.eggmoney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_info.*
 
 class InfoActivity : AppCompatActivity() {
+    //firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+
+        //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            //'R.string.default_web_client_id' 에는 본인의 클라이언트 아이디를 넣어주시면 됩니다.
+            //저는 스트링을 따로 빼서 저렇게 사용했지만 스트링을 통째로 넣으셔도 됩니다.
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //firebase auth 객체
+        firebaseAuth = FirebaseAuth.getInstance()
+        
+        signout_button.setOnClickListener{signOut()}
+    }
+
+
+    private fun signOut() { // 로그아웃
+        // Firebase sign out
+        firebaseAuth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            //updateUI(null)
+        }
     }
 }
