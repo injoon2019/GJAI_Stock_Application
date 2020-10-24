a78727a7c98d78f16bf4ba483cfc2654b37e1527
unknown <kbp4154@naver.com>
2020-10-24 16:52:22 +0900

    자동완성 검색 기능 추가

--- app/src/main/java/com/example/eggmoney/InfoActivity.kt
+++ app/src/main/java/com/example/eggmoney/InfoActivity.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatViewInflater
@@ -36,6 +37,11 @@ class InfoActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        var suggestion = arrayOf("삼성전자","LG전자","나무기술")  // 자동완성 검색기능 부분
+        var adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,suggestion)
+        autocomplete_stock.threshold = 2
+        autocomplete_stock.setAdapter(adapter)
 
 
         setSupportActionBar(info_layout_toolbar) // 툴바를 액티비티의 앱바로 지정
