a619e6645a8baa78684a24bd9a1cbf3795ab09da
unknown <kbp4154@naver.com>
2020-10-28 14:35:19 +0900

    메시지 보내기 구현

--- app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
+++ app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
@@ -1,16 +1,22 @@
 package com.example.eggmoney
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
-import android.database.Cursor
+
 import android.net.Uri
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.TextView
+
 import kotlinx.android.synthetic.main.activity_search_result.*
 
 
 class SearchResultActivity : AppCompatActivity() {
+    private var sendMsg = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
@@ -22,8 +28,6 @@ class SearchResultActivity : AppCompatActivity() {
             intent.setData(Uri.parse("content://com.android.contacts/data/phones"));
             startActivityForResult(intent, 10);
 
-//            var id:String = Uri.parse(data.getDataString()).getLastPathSegment();
-
 
             // TODO: 전화번호부 가져오기
             // TODO: 서버에게 stock_name 혹은 종목코드로 선물 요청 보내기
@@ -33,9 +37,42 @@ class SearchResultActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            sendSMS(data!!, sendMsg)
+        }
+
         super.onActivityResult(requestCode, resultCode, data)
 
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun sendSMS(data: Intent, msg: String) {
+
+        with(contentResolver) {
+            data.data?.let {
+                query(
+                    it, arrayOf(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    ), null, null, null
+                )
+            }
+        }?.apply {
+            moveToFirst()
+            val name = getString(0)     //0은 이름을 얻어옵니다.
+            val number = getString(1)   //1은 번호를 받아옵니다.
+
+            close()
+
+            (findViewById<View>(R.id.activity_search_gift_button) as TextView).text = "name : $name number : $number"
+
+            val n = Uri.parse("smsto: $number")
+            val intent = Intent(Intent.ACTION_SENDTO, n)
+            intent.putExtra("sms_body", msg)
+            startActivity(intent)
+        }
+    }
 }
+
 
 
