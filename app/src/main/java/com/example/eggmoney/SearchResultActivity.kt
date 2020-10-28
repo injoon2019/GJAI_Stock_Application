8562495fe168c2df0331edd74a0e581a219ac2fa
Injun <ison@sfu.ca>
2020-10-28 11:39:53 +0900

    전화번호부 연동 2

--- app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
+++ app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
@@ -33,12 +33,20 @@ class SearchResultActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_PICK);
             intent.setData(Uri.parse("content://com.android.contacts/data/phones"));
             startActivityForResult(intent, 10);
+            
+//            var id:String = Uri.parse(data.getDataString()).getLastPathSegment();
+
 
             // TODO: 전화번호부 가져오기
             // TODO: 서버에게 stock_name 혹은 종목코드로 선물 요청 보내기
             // TODO: 난수 코드 받기
             // TODO: 전화번호 받으면 그걸로 SMS로 난수코드 보내기
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 }
 
