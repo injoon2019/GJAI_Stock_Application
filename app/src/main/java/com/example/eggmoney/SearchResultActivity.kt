3fa71cac7dd216473507fb853625c5c2a6776143
Injun <ison@sfu.ca>
2020-10-28 11:34:38 +0900

    전화번호부 연동 성공

--- app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
+++ app/src/main/java/com/example/eggmoney/SearchResultActivity.kt
@@ -1,8 +1,26 @@
 package com.example.eggmoney
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_search_result.*
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.loader.app.LoaderManager
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
+import androidx.loader.content.Loader
+import kotlinx.android.synthetic.main.info_toolbar.*
+
 
 class SearchResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +28,18 @@ class SearchResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search_result)
 
         stock_name.text = intent.getStringExtra("stock_name")
+
+        activity_search_gift_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK);
+            intent.setData(Uri.parse("content://com.android.contacts/data/phones"));
+            startActivityForResult(intent, 10);
+
+            // TODO: 전화번호부 가져오기
+            // TODO: 서버에게 stock_name 혹은 종목코드로 선물 요청 보내기
+            // TODO: 난수 코드 받기
+            // TODO: 전화번호 받으면 그걸로 SMS로 난수코드 보내기
+        }
     }
 }
+
+
