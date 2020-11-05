e52469de4827213015bd1b1c8bc6cdfb49a74f0c
unknown <kbp4154@naver.com>
2020-11-05 17:06:39 +0900

    17.06 버전 저장 병표

--- app/src/main/java/com/example/eggmoney/NotificationActivity.kt
+++ app/src/main/java/com/example/eggmoney/NotificationActivity.kt
@@ -5,28 +5,32 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_notification.*
+import kotlinx.android.synthetic.main.info_main_layout.*
+import kotlinx.android.synthetic.main.notification_item_recycler.*
 
 class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        val data:MutableList<notificationItem> = loadData()
+        //val data:MutableList<notificationItem> = loadData()
         var adapter = NotifucationCustomAdapter()
-        adapter.listData = data
-        notification_recyclerView.adapter = adapter
-        notification_recyclerView.layoutManager = LinearLayoutManager(this)
-    }
+        //adapter.listData = data
 
-    fun loadData(): MutableList<notificationItem>{
-        val data:MutableList<notificationItem> = mutableListOf()
-        for (no in 1..10){
-            var name = "병표 님이"
-            var title = "선물하신 카카오 주식을 받으셨습니다"
-            val date = System.currentTimeMillis()
-            var item = notificationItem(name, title, date)
-            data.add(item)
+        present_notification_main_Recycle.adapter = adapter
+        //present_notification_main_Recycle.layoutManager = LinearLayoutManager(this)
+        setContentView(R.layout.notification_item_recycler)
+
+        fun loadData(): MutableList<notificationItem> {
+            val data: MutableList<notificationItem> = mutableListOf()
+            for (no in 1..10) {
+                var name = "병표 님이"
+                var title = "선물하신 카카오 주식을 받으셨습니다"
+                val date = System.currentTimeMillis()
+                var item = notificationItem(name, title, date)
+                data.add(item)
+            }
+            return data
         }
-        return data
     }
 }
