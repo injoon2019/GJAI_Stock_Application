5608ab22f69e1f334ae552b6a1121e6d738635a7
unknown <kbp4154@naver.com>
2020-11-07 23:07:47 +0900

    11.07일 선물알림창 완성

--- app/src/main/java/com/eggmoney/scone/NotificationActivity.kt
+++ app/src/main/java/com/eggmoney/scone/NotificationActivity.kt
@@ -5,31 +5,195 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_notification.*
 import kotlinx.android.synthetic.main.info_main_layout.*
-import kotlinx.android.synthetic.main.notification_item_recycler.*
+
 
 class NotificationActivity : AppCompatActivity() {
+    private var news_list: Int = 0
+    var notification_List = arrayListOf<NotificationItem>(
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "아이유님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "11월 05일 11:50"
+        ),
+        NotificationItem(
+            "빅히트 (352820 코스피)가 등록되었습니다.",
+            "6개월 후 매도 주문이 가능합니다.",
+            "(메시지를 확인해 보세요.)",
+            "11월 01일 17:50"
+        ),
+        NotificationItem(
+            "효성 ITX(094280 코스피)가 등록되었습니다",
+            "즉시 매도 주문이 가능합니다.",
+            "(메시지를 확인해 보세요.)",
+            "10월 28일 09:30"
+        ),
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "장범준님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "10월 25일 08:25"
+        ),
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "판테온님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "10월 21일 11:10"
+        ),
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "장규리님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "10월 19일 19:30"
+        ),
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "이새롬님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "10월 16일 10:32"
+        ),
+        NotificationItem(
+            "쿠폰이 전해졌습니다",
+            "백종원님께서 조커님이 보내신 선물을 받았습니다.",
+            "(감사인사 받기를 놓치지 마세요.)",
+            "10월 14일 15:58"
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        //val data:MutableList<notificationItem> = loadData()
-        var adapter = NotifucationCustomAdapter()
-        //adapter.listData = data
+        var nC_adapter = NotifucationCustomAdapter(this, notification_List)
+        present_notification_main_Recycle.adapter = nC_adapter
 
-        present_notification_main_Recycle.adapter = adapter
-        //present_notification_main_Recycle.layoutManager = LinearLayoutManager(this)
-        setContentView(R.layout.notification_item_recycler)
+        val lm = LinearLayoutManager(this)
+        present_notification_main_Recycle.layoutManager = lm
+        present_notification_main_Recycle.setHasFixedSize(true)
 
-        fun loadData(): MutableList<notificationItem> {
-            val data: MutableList<notificationItem> = mutableListOf()
-            for (no in 1..10) {
-                var name = "병표 님이"
-                var title = "선물하신 카카오 주식을 받으셨습니다"
-                val date = System.currentTimeMillis()
-                var item = notificationItem(name, title, date)
-                data.add(item)
+        srl_notification.setOnRefreshListener {
+            // 사용자가 아래로 드래그 했다가 놓았을 때 호출 됩니다.
+            // 이때 새로고침 화살표가 계속 돌아갑니다.
+            // 서버통신~~
+            var notification_List_1 = arrayListOf<NotificationItem>(
+                NotificationItem(
+                    "쿠폰이 전해졌습니다",
+                    "아이유님께서 조커님이 보내신 선물을 받았습니다.",
+                    "(감사인사 받기를 놓치지 마세요.)",
+                    "11월 05일 11:50"
+                )
+            )
+            if (news_list == 0) {
+                notification_List_1 = arrayListOf<NotificationItem>(
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "정인선님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "11월 07일 16:05"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "아이유님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "11월 05일 11:50"
+                    ),
+                    NotificationItem(
+                        "빅히트 (352820 코스피)가 등록되었습니다.",
+                        "6개월 후 매도 주문이 가능합니다.",
+                        "(메시지를 확인해 보세요.)",
+                        "11월 01일 17:50"
+                    ),
+                    NotificationItem(
+                        "효성 ITX(094280 코스피)가 등록되었습니다",
+                        "즉시 매도 주문이 가능합니다.",
+                        "(메시지를 확인해 보세요.)",
+                        "10월 28일 09:30"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "장범준님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 25일 08:25"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "판테온님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 21일 11:10"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "장규리님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 19일 19:30"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "이새롬님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 16일 10:32"
+                    )
+                )
+                news_list = 1
+            } else {
+                notification_List_1 = arrayListOf<NotificationItem>(
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "조보아님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "11월 10일 13:15"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "정인선님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "11월 07일 16:05"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "아이유님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "11월 05일 11:50"
+                    ),
+                    NotificationItem(
+                        "빅히트 (352820 코스피)가 등록되었습니다.",
+                        "6개월 후 매도 주문이 가능합니다.",
+                        "(메시지를 확인해 보세요.)",
+                        "11월 01일 17:50"
+                    ),
+                    NotificationItem(
+                        "효성 ITX(094280 코스피)가 등록되었습니다",
+                        "즉시 매도 주문이 가능합니다.",
+                        "(메시지를 확인해 보세요.)",
+                        "10월 28일 09:30"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "장범준님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 25일 08:25"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "판테온님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 21일 11:10"
+                    ),
+                    NotificationItem(
+                        "쿠폰이 전해졌습니다",
+                        "장규리님께서 조커님이 보내신 선물을 받았습니다.",
+                        "(감사인사 받기를 놓치지 마세요.)",
+                        "10월 19일 19:30"
+                    )
+                )
+                news_list = 0
             }
-            return data
+
+
+            var SW_adapter = NotifucationCustomAdapter(this, notification_List_1)
+            present_notification_main_Recycle.adapter = SW_adapter
+            srl_notification.isRefreshing = false //서버 통신 완료 후 호출해줍니다.
         }
     }
+
 }
--- app/src/main/java/com/eggmoney/scone/notificationItem.kt
+++
@@ -1,3 +0,0 @@
-package com.eggmoney.scone
-
-data class notificationItem(var name: String, var text: String, var timestamp: Long)
